29befc7065c8c8f3614f2e037b4f256544fbe496
Monty Pages <matmontypages@gmail.com>
2026-03-09 01:32:48 -0700

    when plus button is clicked, plus turns to minus and all other answers are closed

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 /*
 TODO:
 -animate opening
--change plus icon to minus
--when one answer is open, close all others
 -add bg image
 */
 
 
 
 const FAQs = document.querySelectorAll('.faq');
-FAQs.forEach(function(faq, i) {
+FAQs.forEach(function(faq, index) {
     const btn = faq.querySelector('img');
     const ans = faq.querySelector('.a');
 
     btn.addEventListener('click', () => {
-        ans.classList.toggle('open');
+        for(let i = 0; i < FAQs.length; i++) {
+            if (i !== index) {
+                const otherBtn = FAQs[i].querySelector('img');
+                otherBtn.src = 'assets/images/icon-plus.svg';
+                FAQs[i].classList.remove('open');
+            }
+        }
+        faq.classList.toggle('open');
+        btn.src = faq.classList.contains('open')
+            ? 'assets/images/icon-minus.svg' 
+            : 'assets/images/icon-plus.svg';
     })
 })
